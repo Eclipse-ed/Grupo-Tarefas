9f1129212ddf6f4524bd6e6bd4f77e7cfb600960
GustavoVieck <gustavo.dores@sptech.school>
2025-06-11 19:10:23 -0300

    Script KPI

--- B.D/Tabelas_BD.sql
+++ B.D/Tabelas_BD.sql
@@ -172,3 +172,39 @@ ON
 WHERE p.idPlantacao = 1
 group by p.nome;
 	  
+
+
+    
+SELECT
+	   p.idPlantacao as 'identificadorPlantacao',
+	   MAX(r.lux) AS 'maiorlux', 
+	   day(r.dtHora) as 'dia'
+FROM plantacao p join sensor s
+on p.idPlantacao = s.fkPlantacao
+JOIN registro r 
+on r.fkSensor = s.idSensor
+where idPlantacao = 1
+group by p.idPlantacao, day(r.dtHora);
+
+SELECT
+	   p.idPlantacao as 'identificadorPlantacao',
+	   MIN(r.lux) AS 'menorlux', 
+	   day(r.dtHora) as 'dia'
+FROM plantacao p join sensor s
+on p.idPlantacao = s.fkPlantacao
+JOIN registro r 
+on r.fkSensor = s.idSensor
+where idPlantacao = 1
+group by p.idPlantacao, day(r.dtHora);
+
+
+SELECT
+	   p.idPlantacao as 'identificadorPlantacao',
+	   round(avg(r.lux),1) AS 'medialux', 
+	   day(r.dtHora) as 'dia'
+FROM plantacao p join sensor s
+on p.idPlantacao = s.fkPlantacao
+JOIN registro r 
+on r.fkSensor = s.idSensor
+where idPlantacao = 1
+group by p.idPlantacao, day(r.dtHora);
